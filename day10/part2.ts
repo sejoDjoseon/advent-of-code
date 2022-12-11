d5181d5cf979de50f530e1572bf71869695a07c5
sejoDjoseon <sergio@reby.co>
2022-12-11 14:24:02 +0100

    day10

---
+++ day10/part2.ts
@@ -0,0 +1,93 @@
+import * as fs from "fs";
+import { EOL } from "os";
+
+let lines = fs.readFileSync("input.txt", "utf-8").split(EOL);
+
+enum Instruction {
+    Noop = "noop",
+    Add = "addx",
+}
+
+class Process {
+    intruction: Instruction
+    cycles: number
+    clocks: number = 0
+
+    constructor(instruction: Instruction) {
+        this.intruction = instruction;
+        this.cycles = this.instructionCycles(instruction)
+    }
+
+    private instructionCycles(instruction: Instruction): number {
+        return {
+            [Instruction.Add]: 2,
+            [Instruction.Noop]: 1
+        }[instruction]
+    }
+
+    tick() {
+        this.clocks++
+    }
+
+    isFinished(): boolean {
+        return this.cycles === this.clocks
+    }
+
+    perform(): (((param: number, register: number) => number)) {
+        return {
+            [Instruction.Add]: (param: number, register: number) => (param + register),
+            [Instruction.Noop]: (_param: number, register: number) => register
+        }[this.intruction]
+    }
+}
+
+function parseInstruction(line: string): { inst: Instruction, value: number } {
+    const [inst, value] = line.split(' ')
+    return { inst: inst as Instruction, value: !value ? 0 : parseInt(value) }
+}
+
+let currentClock = 0
+let registerX = 1
+let instructionIndex = 0
+
+const { inst, value } = parseInstruction(lines[instructionIndex])
+let currentInstruction = new Process(inst)
+let currentParam = value
+
+let signalStrengths: number[] = []
+
+const readInstruction = () => {
+    if (!!lines[instructionIndex]) {
+        const { inst, value } = parseInstruction(lines[instructionIndex])
+        currentInstruction = new Process(inst)
+        currentParam = value
+    }
+}
+
+const printPixel = () => {
+    let pixel: string
+    const mod40 = currentClock % 40
+    if (mod40 >= registerX && mod40 <= registerX + 2) {
+        pixel = '#'
+    } else {
+        pixel = '.'
+    }
+
+    process.stdout.write(pixel)
+    if (mod40 === 0) {
+        process.stdout.write('\n')
+    }
+}
+
+while (instructionIndex < lines.length) {
+    currentClock++
+    currentInstruction.tick()
+    signalStrengths[currentClock - 1] = currentClock * registerX
+    printPixel()
+    if (currentInstruction.isFinished()) {
+        const action = currentInstruction.perform()
+        registerX = action(currentParam, registerX)
+        instructionIndex++
+        readInstruction()
+    }
+}
